8bde49ddea5e4bb03c302811e08b400bcb1fc38d
Santiago Julian <sjulian983@gmail.com>
2022-02-02 11:48:59 +0100

    version 2.0

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const number = document.querySelector(".num");
 const textHandler = (e) => {
   let count = 0;
 
-  let phrase = e.target.value;
-  phrase.toLowerCase();
+  let phrase = e.target.value.toLowerCase();
   let arr = phrase.split("");
   arr.forEach((char) => {
     if (
@@ -16,6 +15,24 @@ const textHandler = (e) => {
       char == "u"
     ) {
       count++;
+      setTimeout(() => {
+        number.style.color = "black";
+        number.style.fontSize = "2.5vw";
+        number.style.backgroundColor = "#e6e6ec";
+        number.style.width = "7vw";
+        number.style.height = "7vw";
+      }, 100);
+      number.style.color = "#5bccf6";
+      number.style.fontSize = "3.5vw";
+      number.style.backgroundColor = "#fcde67";
+      number.style.width = "9vw";
+      number.style.height = "9vw";
+    } else {
+      number.style.color = "black";
+      number.style.fontSize = "2.5vw";
+      number.style.backgroundColor = "#e6e6ec";
+      number.style.width = "7vw";
+      number.style.height = "7vw";
     }
   });
   number.textContent = count;
